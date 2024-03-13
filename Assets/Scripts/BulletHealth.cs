4f862c0034f76e804aaa6534786ceb30ba01d544
Chezaus <109493757+Chezaus@users.noreply.github.com>
2024-03-13 14:10:43 -0400

    pathfinding
    -added pathfinding
    -bullet penetration

--- Assets/Scripts/BulletHealth.cs
+++ Assets/Scripts/BulletHealth.cs
@@ -6,11 +6,28 @@ public class BulletHealth : MonoBehaviour
 {
 
     [SerializeField] public int bulletPenetration;
+    [SerializeField] public LayerMask layers;
+
+    private int health;
+
+    void Start()
+    {
+        health = bulletPenetration;
+    }
 
     void OnCollisionEnter2D(Collision2D other)
     {
-        Destroy(this.gameObject);
+        if(other.gameObject.layer != layers)
+        {
+            health -= 1;
+        }
+
+        if(health <= 0)
+        {
+            Destroy(this.gameObject);
+        }
     }
+
             
 
 }    

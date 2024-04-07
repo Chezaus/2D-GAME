72aa3045e471cfdbf5d0bd01c2c75583e3cdecff
Chezaus <109493757+Chezaus@users.noreply.github.com>
2024-04-07 14:01:32 -0400

    better bullet
    -added bullet art and rotation
    -background is now black

--- Assets/Scripts/PlayerAim.cs
+++ Assets/Scripts/PlayerAim.cs
@@ -10,6 +10,7 @@ public class PlayerAim : MonoBehaviour
     [SerializeField] public float fireRate;
 
     private float cooldown;
+    private float angle;
 
     void Update()
     {
@@ -20,6 +21,8 @@ public class PlayerAim : MonoBehaviour
             
                 GameObject recentBullet = (GameObject)Instantiate(bullet, this.transform.position, Quaternion.identity);
                 recentBullet.GetComponent<Rigidbody2D>().velocity = new Vector2 (Camera.main.ScreenToWorldPoint(Input.mousePosition).x - character.gameObject.transform.position.x,Camera.main.ScreenToWorldPoint(Input.mousePosition).y - character.gameObject.transform.position.y).normalized * bulletSpeed;
+                recentBullet.transform.eulerAngles = new Vector3 (0, 0, angle-90);
+                
                 cooldown = 0f;
             }
         }     
@@ -27,6 +30,12 @@ public class PlayerAim : MonoBehaviour
 
     void FixedUpdate()
     {
+        Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        
+        Vector2 direction = mousePosition - transform.position;
+        angle = Vector2.SignedAngle(Vector2.right, direction);
+        transform.eulerAngles = new Vector3 (0, 0, angle-90);
+
         if(cooldown < fireRate)
         {
             cooldown += Time.fixedDeltaTime;
